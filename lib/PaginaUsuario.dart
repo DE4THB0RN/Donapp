0c339984a2afb07588652372d912c0ed2bcb1d73
Pedro Augusto <127454807+DE4THB0RN@users.noreply.github.com>
2024-09-23 23:19:41 -0300

    Pagina de usuario estática feita

--- lib/PaginaUsuario.dart
+++ lib/PaginaUsuario.dart
@@ -12,7 +12,7 @@ class _PaginausuarioState extends State<Paginausuario> {
   Widget build(BuildContext context) {
     return Column(children: [
       Padding(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: Center(
           child: CircleAvatar(
             radius: 56,
@@ -26,6 +26,83 @@ class _PaginausuarioState extends State<Paginausuario> {
           ),
         ),
       ),
+      Center(
+        child: Text(
+          'Usuário',
+          style: TextStyle(
+              fontSize: 26, color: Colors.white, fontFamily: 'Katibeh'),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Text(
+            '300 seguindo',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(40),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Text(
+            'Histórico de doações',
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontFamily: 'Katibeh'),
+          ),
+        ),
+      ),
+      Center(
+        child: Padding(
+          padding: EdgeInsets.all(3),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Card(
+                child: SizedBox(
+                  width: 300,
+                  child: Padding(
+                    padding: EdgeInsets.all(2),
+                    child: Column(children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: EdgeInsets.all(3),
+                          child: Row(
+                            children: [
+                              Text(
+                                'ONG 1',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      75, 0, 0, 0)),
+                              Text(
+                                '23/02',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: EdgeInsets.all(2),
+                          child: Text("R\$ XX,##"),
+                        ),
+                      ),
+                    ]),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      )
     ]);
   }
 }
