ef7167a6209b8ed18eda36a66ab72ac4291ff734
Gustavo Garcia Macedo <130414880+GustavoNRD@users.noreply.github.com>
2024-10-29 20:40:59 -0300

    Update sql_treco.dart

--- lib/BD/sql_treco.dart
+++ lib/BD/sql_treco.dart
@@ -1,66 +1,70 @@
 import 'package:sqflite/sqflite.dart' as sql;
 
 class SQLHelper {
-  static Future<void> criaTabela(sql.Database database) async {
-    await database.execute("""CREATE TABLE produtos(
+  static Future<void> criaUsuario(sql.Database database) async {
+    await database.execute("""CREATE TABLE usuario(
  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
  nome TEXT,
- valor REAL,
- ean INTEGER,
- qte INTEGER,
- createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+ dataNasc DATE,
+ email TEXT,
+ senha INTEGER
  )
  """);
   }
 
   static Future<sql.Database> db() async {
     return sql.openDatabase(
-      'produtos.db',
+      'usuario.db',
       version: 1,
       onCreate: (sql.Database database, int version) async {
-        await criaTabela(database);
+        await criaUsuario(database);
       },
     );
   }
 
-  static Future<int> adicionarProduto(
-      String nome, double valor, int ean, int qte) async {
+  static Future<int> adicionarUsuario(
+      String nome, String dataNasc, String email, int senha) async {
     final db = await SQLHelper.db();
-    final dados = {'nome': nome, 'valor': valor, 'ean': ean, 'qte': qte};
-    final id = await db.insert('produtos', dados,
+    final dados = {
+      'nome': nome,
+      'dataNasc': dataNasc,
+      'email': email,
+      'senha': senha
+    };
+    final id = await db.insert('usuario', dados,
         conflictAlgorithm: sql.ConflictAlgorithm.replace);
     return id;
   }
 
-  static Future<List<Map<String, dynamic>>> pegaProdutos() async {
+  static Future<List<Map<String, dynamic>>> pegaUsuario() async {
     final db = await SQLHelper.db();
-    return db.query('produtos', orderBy: "id");
+    return db.query('usuario', orderBy: "id");
   }
 
-  static Future<List<Map<String, dynamic>>> pegaUmProduto(int id) async {
+  static Future<List<Map<String, dynamic>>> pegaUmUsuario(int id) async {
     final db = await SQLHelper.db();
-    return db.query('produtos', where: "id = ?", whereArgs: [id], limit: 1);
+    return db.query('usuario', where: "id = ?", whereArgs: [id], limit: 1);
   }
 
-  static Future<int> atualizaProduto(
-      int id, String nome, double valor, int ean, int qte) async {
+  static Future<int> atualizaUsuario(
+      int id, String nome, String dataNasc, String email, int senha) async {
     final db = await SQLHelper.db();
     final dados = {
       'nome': nome,
-      'valor': valor,
-      'ean': ean,
-      'qte': qte,
+      'dataNasc': dataNasc,
+      'email': email,
+      'senha': senha,
       'createdAt': DateTime.now().toString()
     };
     final result =
-        await db.update('produtos', dados, where: "id = ?", whereArgs: [id]);
+        await db.update('usuario', dados, where: "id = ?", whereArgs: [id]);
     return result;
   }
 
-  static Future<void> apagaProduto(int id) async {
+  static Future<void> apagaUsuario(int id) async {
     final db = await SQLHelper.db();
     try {
-      await db.delete("produtos", where: "id = ?", whereArgs: [id]);
+      await db.delete("usuario", where: "id = ?", whereArgs: [id]);
     } catch (err) {
       print("Erro ao apagar o item item: $err");
     }
