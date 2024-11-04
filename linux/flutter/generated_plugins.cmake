62cc891b183e932e1ac1f0271a056aece4311a2d
IgorPaschoalino <oigor.paschoalino@gmail.com>
2024-11-04 19:05:41 -0300

    slakkkkkkkk

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   encrypt_decrypt_plus
+  file_selector_linux
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <encrypt_decrypt_plus/encrypt_decrypt_plus_plugin_c_api.h>
+#include <file_selector_windows/file_selector_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   EncryptDecryptPlusPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("EncryptDecryptPlusPluginCApi"));
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
 }
