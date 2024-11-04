9cd4b2d403c273e8408c64b442225da781338af2
Gustavo Garcia Macedo <130414880+GustavoNRD@users.noreply.github.com>
2024-11-04 18:49:00 -0300

    teste

--- lib/Telas/Cadastro2ong.dart
+++ lib/Telas/Cadastro2ong.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:donapp/Components/ImageInputField.dart';
 import 'package:donapp/Theme/Color.dart';
 import 'package:flutter/material.dart';
@@ -19,7 +21,7 @@ class _Cadastro2OngState extends State<Cadastro2Ong> {
   String CNPJ = '';
   String senha = '';
   List<String> localidades = [];
-  List<TextEditingController> _controllers = []; // Lista de controladores para cada campo
+  List<TextEditingController> _controllers = [];
 
   void _initPrefs() async {
     prefs = await SharedPreferences.getInstance();
@@ -33,8 +35,7 @@ class _Cadastro2OngState extends State<Cadastro2Ong> {
 
   void _saveLocalidades() {
     localidades = _controllers.map((controller) => controller.text).toList();
-    // Aqui, você pode salvar as localidades no banco de dados ou onde desejar.
-    print(localidades); // Para verificar o conteúdo das localidades
+    print(localidades);
   }
 
   @override
@@ -45,103 +46,150 @@ class _Cadastro2OngState extends State<Cadastro2Ong> {
     super.dispose();
   }
 
+  Uint8List? _base64ToImage(String base64String) {
+    try {
+      return Base64Decoder().convert(base64String);
+    } catch (e) {
+      print("Erro ao converter base64 para imagem: $e");
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.backgroundColor,
       body: SingleChildScrollView(
         child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: AppColor.appBarColor,
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Imagem de Perfil',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 15),
-                    ImageInputField(
-                      onImageSelected: (imageString) {
-                        setState(() {
-                          perfil = imageString;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 15),
-                    Text(
-                      'Imagem do Banner',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 15),
-                    ImageInputField(
-                      onImageSelected: (imageString) {
-                        setState(() {
-                          banner = imageString;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 15),
-                    Text(
-                      'Localidades',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Column(
-                      children: _controllers.asMap().entries.map((entry) {
-                        int index = entry.key;
-                        TextEditingController controller = entry.value;
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 5.0),
-                          child: CustomInputField(
-                            labelText: 'Localidade ${index + 1}',
-                            hintText: 'Digite a localidade',
-                            keyboardType: TextInputType.text,
-                            onChanged: (value) {
-                              controller.text = value;
-                            },
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    color: AppColor.appBarColor,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Imagem de Perfil',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
                           ),
-                        );
-                      }).toList(),
+                        ),
+                        SizedBox(height: 15),
+                        ImageInputField(
+                          onImageSelected: (imageString) {
+                            setState(() {
+                              perfil = imageString;
+                              print(perfil);
+                            });
+                          },
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          'Imagem do Banner',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        ImageInputField(
+                          onImageSelected: (imageString) {
+                            setState(() {
+                              banner = imageString;
+                            });
+                          },
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          'Localidades',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Column(
+                          children: _controllers.asMap().entries.map((entry) {
+                            int index = entry.key;
+                            TextEditingController controller = entry.value;
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 5.0),
+                              child: CustomInputField(
+                                labelText: 'Localidade ${index + 1}',
+                                hintText: 'Digite a localidade',
+                                keyboardType: TextInputType.text,
+                                onChanged: (value) {
+                                  controller.text = value;
+                                },
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                        SizedBox(height: 10),
+                        CustomButton(
+                          text: 'Adicionar Localidade',
+                          onPressed: _addLocalidade,
+                        ),
+                        SizedBox(height: 10),
+                        CustomButton(
+                          text: 'Terminar',
+                          onPressed: () {
+                            _saveLocalidades();
+                            Navigator.pushReplacementNamed(context, 'Login');
+                          },
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 10),
-                    CustomButton(
-                      text: 'Adicionar Localidade',
-                      onPressed: _addLocalidade,
-                    ),
-                    SizedBox(height: 10),
-                    CustomButton(
-                      text: 'Terminar',
-                      onPressed: () {
-                        _saveLocalidades();
-                        Navigator.pushReplacementNamed(context, 'Login');
-                      },
-                    ),
-                  ],
+                  ),
                 ),
               ),
-            ),
+              if (perfil.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Pré-visualização da Imagem de Perfil',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        _base64ToImage(perfil) != null
+                            ? Image.memory(
+                                _base64ToImage(perfil)!,
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.cover,
+                              )
+                            : Text("Erro ao carregar a imagem"),
+                      ],
+                    ),
+                  ),
+                ),
+            ],
           ),
         ),
       ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import encrypt_decrypt_plus
+import file_selector_macos
 import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   EncryptDecryptPlusPlugin.register(with: registry.registrar(forPlugin: "EncryptDecryptPlusPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
