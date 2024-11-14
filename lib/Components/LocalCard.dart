c2129f8209379eb13f16ce7ac835738eb474a7b4
Gustavo Garcia Macedo <130414880+GustavoNRD@users.noreply.github.com>
2024-11-14 00:54:00 -0300

    tudo funcionando legal

---
+++ lib/Components/LocalCard.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:donapp/Theme/Color.dart';
+
+class LocalCard extends StatelessWidget {
+  final String rua;
+  final String bairro;
+  final String complemento;
+
+  const LocalCard({
+    required this.rua,
+    required this.bairro,
+    required this.complemento,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12.0),
+      margin: const EdgeInsets.symmetric(vertical: 5.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Rua: $rua',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: AppColor.appBarColor,
+            ),
+          ),
+          Text(
+            'Bairro: $bairro',
+            style: TextStyle(
+              fontSize: 14,
+              color: AppColor.appBarColor,
+            ),
+          ),
+          if (complemento.isNotEmpty)
+            Text(
+              'Complemento: $complemento',
+              style: TextStyle(
+                fontSize: 14,
+                color: AppColor.appBarColor,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
