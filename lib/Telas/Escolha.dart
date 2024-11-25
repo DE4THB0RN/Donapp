7bcfbd2b030e4e4f3bafc27641996f4298486ae4
Gustavo Garcia Macedo <130414880+GustavoNRD@users.noreply.github.com>
2024-11-25 17:07:05 -0300

    varias mudança

--- lib/Telas/Escolha.dart
+++ lib/Telas/Escolha.dart
@@ -1,5 +1,8 @@
 import 'package:donapp/BD/sql_ONG.dart';
 import 'package:donapp/BD/sql_local_ONG.dart';
+import 'package:donapp/BD/sql_user.dart';
+import 'package:donapp/Components/GenericImages.dart';
+import 'package:donapp/Components/Helper.dart';
 import 'package:donapp/Theme/Color.dart';
 import 'package:flutter/material.dart';
 
@@ -32,16 +35,52 @@ class _EscolhaState extends State<Escolha> {
     );
   }
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   dores();
-  // }
+  @override
+  void initState() {
+    super.initState();
+    //dores();
+    //iniciarOngsMock();
+  }
 
-  // void dores() async {
-  //   await SQLLocal.dropDataBaseLocal();
-  //   await SQLONG.dropDataBaseONG();
-  // }
+  void dores() async {
+    await SQLLocal.dropDataBaseLocal();
+    await SQLONG.dropDataBaseONG();
+    await SQLUser.dropDataBaseUser();
+  }
+
+  void iniciarOngsMock() async {
+    SQLONG.adicionarONG("", "ongsaude@gmail.com", "", "");
+    SQLONG.atualizaONG(
+        await SQLONG.pegaIdOng("ongsaude@gmail.com"),
+        "ONG SAÚDE",
+        "839289482789",
+        "ongsaude@gmail.com",
+        generateMd5("senha"),
+        "descrição",
+        await genericProfile(),
+        await genericBanner());
+    SQLONG.adicionarONG("", "ongcomida@gmail.com", "", "");
+    SQLONG.atualizaONG(
+        await SQLONG.pegaIdOng("ongcomida@gmail.com"),
+        "ONG COMIDA",
+        "6466456464556",
+        "ongcomida@gmail.com",
+        generateMd5("senha"),
+        "descrição",
+        await genericProfile(),
+        await genericBanner());
+
+    SQLONG.adicionarONG("", "ongrefugiados@gmail.com", "", "");
+    SQLONG.atualizaONG(
+        await SQLONG.pegaIdOng("ongrefugiados@gmail.com"),
+        "ONG REFUGIADOS",
+        "3534536643656",
+        "ongrefugiados@gmail.com",
+        generateMd5("senha"),
+        "descrição",
+        await genericProfile(),
+        await genericBanner());
+  }
 }
 
 _botao(context, text, destino) {
