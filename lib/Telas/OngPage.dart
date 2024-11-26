5529bee8706299f400676738fc2be975f409597b
IgorPaschoalino <oigor.paschoalino@gmail.com>
2024-11-26 16:04:52 -0300

    meus trem ai

--- lib/Telas/OngPage.dart
+++ lib/Telas/OngPage.dart
@@ -15,6 +15,8 @@ import 'package:donapp/Components/ButtonEdited.dart';
 import 'package:donapp/Components/PostCard.dart';
 import 'package:donapp/Components/Preencha.dart';
 import 'package:donapp/Components/localClass.dart';
+import 'package:donapp/Components/ConfirmPopup.dart';
+import 'package:donapp/Components/DonationCard.dart';
 import 'package:donapp/Theme/Color.dart';
 import 'package:donapp/Theme/Padding.dart';
 import 'package:flutter/material.dart';
@@ -623,6 +625,77 @@ class _OngpageState extends State<Ongpage> {
         });
   }
 
+  void _openHistoricoPopup(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            backgroundColor: AppColor.appBarColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      icon: const Icon(Icons.close, color: Colors.white),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                  const Text(
+                    'Histórico de Doações',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  const Text(
+                    totalArrecadado,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    'Arrecadados',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(3),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          DoacaoCard(dia: "26/11", valor: 1234.56),
+                          
+                          DoacaoCard(dia: "25/11", valor: 567.89),
+                          
+                          DoacaoCard(dia: "24/11", valor: 34.50),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   void _openConfirmDonationPopup(BuildContext context) {
     showDialog(
       context: context,
@@ -690,79 +763,30 @@ class _OngpageState extends State<Ongpage> {
     );
   }
 
-  void _openConfirmDeletePopup(BuildContext context) {
-    showDialog(
+  void _openConfirmDeletePopup() async {
+    await showConfirmDialog(
       context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          backgroundColor: AppColor.appBarColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          content: const Text(
-            'Tem certeza de que deseja excluir a conta?',
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey, // Cor de fundo para o botão "Sim"
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.pop(
-                          context); // Fecha apenas o pop-up de confirmação
-                    },
-                    child: const Text(
-                      'Não',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red, // Cor de fundo para o botão "Sim"
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: TextButton(
-                    onPressed: () async {
-                      print('Hora de apagar');
-                      String emailONG =
-                          cipher.xorDecode(prefs.getString('email')!);
-                      List<Map<String, dynamic>> Ongfull =
-                          await SQLONG.pegaUmaONGEmail2(emailONG);
-                      // if (id == -1) {
-                      //   id = userfull.first['id'];
-                      // }
+      message: 'Tem certeza de que deseja excluir a conta?',
+      onConfirm: () async {
+        print('Hora de apagar');
+        String emailONG = cipher.xorDecode(prefs.getString('email')!);
+        List<Map<String, dynamic>> OngFull =
+            await SQLONG.pegaUmaONGEmail2(emailONG);
 
-                      // await SQLUser.apagaUsuario(id);
+        // Aqui você pode adicionar sua lógica de exclusão, como descomentar este trecho:
+        // await SQLUser.apagaUsuario(OngFull.first['id']);
+        // await prefs.remove('email');
+        // await prefs.remove('senha');
+        // await prefs.remove('nome');
+        // print('Dados apagados com sucesso!');
 
-                      // SharedPreferences prefs =
-                      //     await SharedPreferences.getInstance();
-
-                      // // Remove os tokens armazenados
-                      // await prefs.remove('email');
-                      // await prefs.remove('senha');
-                      // await prefs.remove('nome');
-                      // print('apagado');
-                      Navigator.pop(context);
-                      Navigator.pop(context); // Fecha o pop-up de confirmação
-                      Navigator.pushReplacementNamed(context, 'Escolha');
-                    },
-                    child: const Text(
-                      'Sim',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        );
+        // Navegar para a tela inicial ou outra ação:
+        Navigator.pop(context); // Fecha o popup
+        Navigator.pushReplacementNamed(context, 'Escolha');
+      },
+      onCancel: () {
+        print('Exclusão cancelada.');
+        Navigator.pop(context);
       },
     );
   }
