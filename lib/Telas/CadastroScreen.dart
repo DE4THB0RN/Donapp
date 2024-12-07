28180021c5a6919d548d00f481dcd9e752d247bd
Souza-git <danielvsouza25@gmail.com>
2024-12-07 08:33:39 -0300

    Update CadastroScreen.dart

--- lib/Telas/CadastroScreen.dart
+++ lib/Telas/CadastroScreen.dart
@@ -156,6 +156,14 @@ class _CadastroScreenState extends State<CadastroScreen> {
                         Navigator.pushReplacementNamed(context, 'Login_User');
                       },
                     ),
+                    SizedBox(height: 20),
+                    // Botão Voltar no final
+                    CustomButton(
+                      text: 'Voltar',
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, 'Escolha');
+                      },
+                    ),
                   ],
                 ),
               ),
